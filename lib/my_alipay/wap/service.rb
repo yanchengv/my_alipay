d31195b0dece343f719fa83ce2aa51e7ea1524b9
yancheng <zqalyc@163.com>
2017-09-14 15:13:15 +0800

    update wap

--- lib/my_alipay/wap/service.rb
+++ lib/my_alipay/wap/service.rb
@@ -2,7 +2,6 @@ module MyAlipay
   module Wap
     module Service
       GATEWAY_URL = 'https://openapi.alipayde.com/gateway.do'
-      #ALIPAYCONFIG = YAML.load_file("config/myalipay.yml")[Rails.env].with_indifferent_access.freeze
 
       #手机支付url
       def self.create_alipay_trade_wap_pay_url(params, options = {})
